2b1c427540e19d9de757ce70b687e5f536e41cac
Ravi3005 <ravinaik9577@gmail.com>
2025-03-22 19:49:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ def trending():
     news = get_trending_news()
     return jsonify({"trending_news": news})
 
+from waitress import serve
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=5000)
+
